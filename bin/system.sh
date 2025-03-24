10a92e1c284eb34eff0662736856f7e0d0b8fe69
root <git@github.com>
2025-03-24 14:56:38 +0300

    v0.3.1

--- bin/system.sh
+++ bin/system.sh
@@ -98,7 +98,6 @@ typescript() {
     if ! command -v node &>/dev/null; then
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
         . ~/.nvm/nvm.sh
-        # nvm install 18
         nvm install 20
         npm install -g npm pnpm yarn nodemon
         echo 'export NODE_OPTIONS="--max-old-space-size=32096"' >> ~/.bashrc
